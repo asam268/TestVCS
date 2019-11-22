c56d4ec5f35d8173a9b8f6cd34813622d3609d5f
Asa Marshall <asam268@gmail.com>
2019-11-22 18:20:57 -0500

    More warmup problems, testing using HelloWorld's main

--- src/Warmup1.java
+++ src/Warmup1.java
@@ -1,5 +1,8 @@
 public class Warmup1 {
 
+    public Warmup1(String str){
+        System.out.println(delDel(str));
+    }
     /*
     The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep
     in if it is not a weekday or we're on vacation. Return true if we sleep in.
@@ -22,5 +25,27 @@ public class Warmup1 {
      */
     public boolean monkeyTrouble(boolean aSmile, boolean bSmile){
         return (aSmile && bSmile) || (!aSmile && !bSmile);
+    }
+
+    /*
+    Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted.
+    Otherwise, return the string unchanged.
+
+    delDel("adelbc") → "abc"
+    delDel("adelHello") → "aHello"
+    delDel("adedbc") → "adedbc"
+     */
+    public String delDel(String str){
+        try {
+            String temp = str.substring(1, 4);
+            if (temp.equalsIgnoreCase("del")) {
+                String s1 = str.substring(0, 1);
+                String s2 = str.substring(4);
+                return s1 + s2;
+            }
+            return str;
+        } catch(StringIndexOutOfBoundsException e){
+            return str;
+        }
     }
 }
